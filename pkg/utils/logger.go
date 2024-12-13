2a7e28436f7852103dc07ab16423b20f4678218b
jaydee94 <janherber@gmx.de>
2024-12-13 21:15:14 +0100

    Add docstrings

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -5,6 +5,9 @@ import (
 	"os"
 )
 
+// CreateLogger initializes and returns a new logger instance.
+// The logger writes to the standard output and prefixes each log entry
+// with "[chartscan]" followed by the date and time.
 func CreateLogger() *log.Logger {
 	return log.New(os.Stdout, "[chartscan] ", log.LstdFlags)
 }
